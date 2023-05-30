e5e66dd516648371d8ce8de9558b1fde1b4278fb
Adam Meza <Adammeza95@gmail.com>
2023-05-30 11:56:32 -0400

    create Card Component

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+export const Card = ({key, name, date, time, number}) => {
+  
+  return (
+    <div key = {key}>
+      <ul>
+        <h2>{name}</h2>
+        <span>{date}</span>
+        <span>{time}</span>
+        <span>{number}</span>
+      </ul>
+    </div>
+  )
+}
