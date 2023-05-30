8661043e6faa731b1d24b603c296e4e846736ab0
Adam Meza <Adammeza95@gmail.com>
2023-05-30 11:54:41 -0400

    Create App component, including fetch call

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Main } from '../Main/Main';
 
-class App extends Component {
-  render() {
+const App = () => {
+
+  const [reservations, setReservations] = useState([])
+
+  let fetchData =  async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/reservations')
+
+      if (response.ok) {
+        const json = await response.json()
+        setReservations(json)
+      } else {
+        throw new Error(response)
+      }
+    } catch {
+      console.error()
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
+        <h1 className='app-title'>  -- Turing Cafe Reservations  --  </h1>
         <div className='resy-form'>
 
         </div>
         <div className='resy-container'>
-          
+          { reservations && <Main data = { reservations} />}
         </div>
       </div>
     )
-  }
 }
 
 export default App;
