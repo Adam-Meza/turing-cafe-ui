ec34ed505a2770a524a3b123325b4b4045d7dd6d
Adam Meza <Adammeza95@gmail.com>
2023-05-30 11:55:54 -0400

    Create Main Component

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Card } from "../Card/Card";
+
+export const Main = ({ data }) => {
+
+  let reservations = data.map(res => {
+    const {id, date, name, time, number} = res
+    return < Card
+    key = { id }
+    date = { date }
+    name = { name }
+    time = { time }
+    numer = { number }
+    />
+})
+
+
+  return ( 
+  <>
+    { reservations }
+  </>
+  
+  )
+}
